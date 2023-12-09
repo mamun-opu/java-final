674f13a16808a3a339cbcdeadce4daa130145c98
abmamun504@gmail.com <abmamun504@gmail.com>
2023-12-09 11:51:43 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
                         new Number(5)
                 )
         );
+
+        ArithmeticExp moEquation = new Modulo(new Number(15),
+                new Number(4));
         System.out.println("Equation: " + equation.toString());
         System.out.println("result: " + equation.evaluate());
+        System.out.println("Equation: " + moEquation.toString());
+        System.out.println("result: " + moEquation.evaluate());
     }
 }
